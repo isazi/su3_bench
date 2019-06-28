707082bea612657ce5cff1902aa400176aa892bc
Doug Doerfler <dwdoerf@lbl.gov>
2019-06-28 09:49:20 -0700

    Cleaned up unused variables

--- c99_complex/lattice.h
+++ c99_complex/lattice.h
@@ -41,14 +41,5 @@ extern int nx,ny,nz,nt;	// lattice dimensions
 /* Some of these global variables are node dependent */
 /* They are set in "make_lattice()" */
 extern  int sites_on_node;              /* number of sites on this node */
-extern  int even_sites_on_node; /* number of even sites on this node */
-extern  int odd_sites_on_node;  /* number of odd sites on this node */
-extern  int number_of_nodes;    /* number of nodes in use */
-extern  int this_node;          /* node number of this node */
-
-/* Vectors for addressing */
-/* Generic pointers, for gather routines */
-#define N_POINTERS 16
-extern char ** gen_pt[N_POINTERS];
 
 #endif
