593802f0251b6fc23bc8b1e82f7c3c748e894730
Doug Doerfler <dwdoerf@lbl.gov>
2019-09-13 07:30:05 -0700

    removing loadProgram() function, leftover from port from OpenCL version

--- c++/mult_su3_nn_bench.cpp
+++ c++/mult_su3_nn_bench.cpp
@@ -23,19 +23,6 @@ typedef std::chrono::system_clock Clock;
 #  define VERBOSE 1  // valid values 0, 1 or 2
 #endif
 
-static inline std::string loadProgram(std::string input)
-{
-    std::ifstream stream(input.c_str());
-    if (!stream.is_open()) {
-        std::cout << "Cannot open file: " << input << std::endl;
-        exit(1);
-    }
-
-     return std::string(
-        std::istreambuf_iterator<char>(stream),
-        (std::istreambuf_iterator<char>()));
-}
-
 int main(int argc, char *argv[])
 {
   // allocate and initialize the working lattices and B link matrix
