6a0ba7f9e79acd3c7e624803021a60fd8c84a460
Douglas Doerfler <dwdoerf@lbl.gov>
2020-08-19 18:44:31 -0400

    AMD hipcc no longer supports std::complex so using MILC_COMPLEX

--- mat_nn_hip.hpp
+++ mat_nn_hip.hpp
@@ -26,9 +26,16 @@ __global__ void k_mat_nn(
     int j = (myThread%36)/9;
     int k = (myThread%9)/3;
     int l = myThread%3;
-    Complx cc;
+    Complx cc = {0.0,0.0};
     for (int m=0;m<3;m++)
+#ifdef MILC_COMPLEX
+      CMULSUM(a[mySite].link[j].e[k][m], b[j].e[m][l], cc);
+    //c[mySite].link[j].e[k][l].real = cc.real;
+    //c[mySite].link[j].e[k][l].imag = cc.imag;
+#else
       cc += a[mySite].link[j].e[k][m] * b[j].e[m][l];
+    //c[mySite].link[j].e[k][l] = cc;
+#endif
     c[mySite].link[j].e[k][l] = cc;
   }
 }
