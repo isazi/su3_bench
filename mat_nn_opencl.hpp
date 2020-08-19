cdaafbcec6ca71a68ea7aecf10996975e5c11578
Douglas Doerfler <dwdoerf@lbl.gov>
2020-08-19 18:43:11 -0400

    AMD OpenCL requires defining CL_TARGET_OPENCL_VERSION when using CL/cl2.hpp

--- mat_nn_opencl.hpp
+++ mat_nn_opencl.hpp
@@ -2,6 +2,7 @@
 // Preferably use the Khronos CL/cl2.hpp C++ definitions
 #define CL_HPP_TARGET_OPENCL_VERSION 120 
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_TARGET_OPENCL_VERSION 120
 #include <CL/cl2.hpp>
 
 #ifndef DEVICE
