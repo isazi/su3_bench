0988ba7e50cdb558f34dcf5d0553f66e04913b7b
Douglas Doerfler <dwdoerf@lbl.gov>
2020-12-09 14:45:49 -0800

    updated Nsight Compute script

--- utilities/run-nsight.sh
+++ utilities/run-nsight.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-export MV2_ENABLE_AFFINITY=0
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
-export OMP_NUM_THREADS=$((SLURM_CPUS_ON_NODE / 2))
-
 set -x
 
+exe=./bench_f32_cuda.exe
 kernel=k_mat_nn
-threads=512
+#threads=128
+threads=4
 
 filename=profile-${kernel}-t${threads}
 skip=1
 count=10
+#sections="--section ComputeWorkloadAnalysis --section MemoryWorkloadAnalysis --section Occupancy --section SpeedOfLight --section SpeedOfLight_RooflineChart"
+sections="--section SpeedOfLight --section SpeedOfLight_RooflineChart --section SchedulerStats --section WarpStateStats --section MemoryWorkloadAnalysis"
 
-srun nv-nsight-cu-cli -o $filename -k $kernel -s $skip -c $count ./bench_f32_cuda.exe -t $threads
+srun nv-nsight-cu-cli -o $filename -k $kernel -s $skip -c $count $sections $exe -t $threads
 
