1f4ff8f4949fef0f533c0ad24c3eb73020336c4a
Isaac Minn <iminn@login22.chn.perlmutter.nersc.gov>
2024-02-14 21:31:23 -0800

    re-add sycl changes

--- mat_nn_dpcpp.hpp
+++ mat_nn_dpcpp.hpp
@@ -6,8 +6,14 @@
 // Sycl requires that kernels be named
 class k_mat_nn;
 
+typedef struct{
+	double d2h_time;
+	double kernel_time;
+	double h2d_time;
+} Profile;
+
 double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, std::vector<site> &c, 
-              const size_t total_sites, const size_t iterations, size_t wgsize, const int target)
+		  const size_t total_sites, const size_t iterations, size_t wgsize, const int target, Profile* profile)
 { 
   // build a list of devices
   std::vector<sycl::platform> platforms = sycl::platform::get_platforms();
@@ -46,6 +52,9 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
 
   std::cout << std::flush;
 
+  auto tstart = Clock::now();
+  auto tprofiling = tstart;
+
   // allocate device memory
   site*       d_a = (site*)       malloc_device(total_sites * sizeof(site), queue);
   su3_matrix* d_b = (su3_matrix*) malloc_device(4 * sizeof(su3_matrix), queue);
@@ -59,6 +68,9 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
   queue.memcpy(d_a, a.data(), a.size() * sizeof(site));
   queue.memcpy(d_b, b.data(), b.size() * sizeof(su3_matrix));
   queue.wait();
+
+  profile->h2d_time = (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
+  tprofiling = Clock::now();
 
   // benchmark loop
   for (size_t iters=0; iters<iterations+warmups; ++iters) {
@@ -96,11 +108,16 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
   queue.wait();
   } // end of iteration loop
 
-  double ttotal = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tstart).count();
+  profile->kernel_time = (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
+  tprofiling = Clock::now();
 
   // Move the result back to the host side vector
   queue.memcpy(c.data(), d_c, c.size() * sizeof(site));
   queue.wait();
+
+  profile->d2h_time= (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
+
+  double ttotal = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tstart).count();
 
   free(d_a, queue);
   free(d_b, queue);
@@ -108,4 +125,3 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
 
   return (ttotal /= 1.0e6);
 } // end of SYCL block
-
